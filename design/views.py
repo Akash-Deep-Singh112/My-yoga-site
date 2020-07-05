ec23d6bb5c9482e7eb46217a00474d5be6e86921
Akash-Deep-Singh112 <akashdeepsingh112@gmail.com>
2020-07-06 00:35:45 +0530

    made register,login,logout system while keeping the page dynamic

--- design/views.py
+++ design/views.py
@@ -1,20 +1,5 @@
 from django.shortcuts import render
 from .models import Destination
 def index(request):
-    dest1=Destination()
-    dest2=Destination()
-    dest3=Destination()
-    dest1.name='Mumbai'
-    dest1.desc='The city that never sleeps'
-    dest1.img='destination_1.jpg'
-    dest1.price=700
-    dest2.name='Delhi'
-    dest2.desc='The city that is India capital'
-    dest2.img='destination_2.jpg'
-    dest2.price=650
-    dest3.name='Chennai'
-    dest3.desc='The metro city of the south'
-    dest3.img='destination_3.jpg'
-    dest3.price=600
-    dests=[dest1,dest2,dest3]
+    dests = Destination.objects.all()
     return render(request,'index.html',{'dests':dests})
